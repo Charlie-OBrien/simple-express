318d06ebafe3526ac6f53d4e35aaa500f2a484af
Charles O'Brien <charlesobriennb@gmail.com>
2022-05-18 15:43:41 -0300

    Punctuation fix and comment fix

--- main2.js
+++ main2.js
@@ -4,8 +4,8 @@ const port = 3001;
 
 app.get("/main2", (req, res) => {
     res.send("Alternative Hello World app");
-});
-
-app.listen(port, () => {
-    console.log(`this is an alternatively written Hello World Express.js app listining on port ${port}`);
+})
+// if ';' is used on line ^above^ then need to add app below before .listen
+.listen(port, () => {
+    console.log(`This is an alternatively written Hello World Express.js app listining on port ${port}`);
 });
